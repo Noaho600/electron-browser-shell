1cbef8785881ca52fc75a70b4be86e0e546d2740
Noaho600 <Futurenow2024@gmail.com>
2025-08-31 10:36:49 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-const Browser = require('./shell/browser/main')
-new Browser()
+const { BrowserWindow } = require('electron');
+let mainWindow = new BrowserWindow({
+  width: 1200,
+  height: 800,
+  webPreferences: {
+    nodeIntegration: true,
+    contextIsolation: false
+  }
+});
+
+// Load your custom start page
+mainWindow.loadFile('app/start.html');
